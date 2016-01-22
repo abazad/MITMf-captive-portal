f8241c4712ea54b81994e059ea3dc6454bfc73f8
camelinc <camelinc@users.noreply.github.com>
2016-01-22 13:00:55 +0100

    updated to version 0.2

--- Portal.py
+++ Portal.py
@@ -8,22 +8,19 @@ from urlparse import urlparse
 logging.getLogger("scapy.runtime").setLevel(logging.ERROR)  #Gets rid of IPV6 Error when importing scapy
 from scapy.all import *
 
-try:
-  from configobj import ConfigObj
-except:
-  sys.exit('[-] configobj library not installed!')
-
 from plugins.plugin import Plugin
 
-#debug
-from pprint import pprint
+# Initialize coloredlogs.
+from pprint import pprint #DEBUG
+import socket
 
+#from netfilterqueue import NetfilterQueue #TODO
 
 class Portal(Plugin):
     name = 'Portal'
     optname = 'portal'
     desc = 'Captive Portal test plugin'
-    version   = "0.1"
+    version   = "0.2"
     tree_info = ["Captive Portal running"]
     has_opts = True
 
@@ -32,251 +29,230 @@ class Portal(Plugin):
         self.ip            = options.ip
         self.mon_interface = options.interface
 
-        self.portal_html   = """ <html>
-      <head>
-         <title>Network Authentication Required</title>
-         <meta http-equiv="refresh"
-               content="0; url=https://fritz.box/">
-      </head>
-      <body>
-         <p>You need to <a href="https://fritz.box/">
-         authenticate with the local network</a> in order to gain
-         access.</p>
-      </body>
-   </html>"""
-        
         #Table for clients not beeing captured
         self.dtable        = {}
+        self.portal_ip     = '127.0.0.1'
+        self.portal_url    = False
+        self.portal_dns    = False
+        self.portal_html   = ''
 
         #TODO: IETF Captive-Portal Identification in DHCP: https://tools.ietf.org/html/draft-wkumari-dhc-capport-16
-        
-        #Block https traffic
-        #self._block_traffic()
-        
-        if options.portalurl:
+
+        if options.portaltemplate:
+            self.portal_html = self.parse_template(options.portaltemplate)
+
+        elif options.portalurl:
             #parse url 
             try:
                 self.portal_url = urlparse(options.portalurl)
                 self.log.debug("Captival portal at {}".format(self.portal_url))
             except Exception, e:
                 sys.exit("[-] Error parsing portal url: %s" % e)
+
+            # get portal url ip address
+            self.portal_ip = socket.gethostbyname('google.com') #TODO: better method, scapy?
+
+            if options.portaldns:
+                #TODO: if options.portaldns
+                t2 = threading.Thread(name='captive_dns_thread', target=self.captive_dns, args=(self.mon_interface,))
+                t2.setDaemon(True)
+                t2.start()
         else:
-            self.portal_url = False
+            sys.exit("[-] DNS records require portal url or portal template: %s" % e)
 
-        try:
-            self.aircfg = ConfigObj("./config/portal.cfg")
-            #TODO: delete me
-            #Here we compile the regexes for faster performance when injecting packets
-            for rule in self.aircfg.items():
-                rule[1]['match'] = re.compile(r'%s' % rule[1]['match'])
-                if 'ignore' in rule[1].keys():
-                    rule[1]['ignore'] = re.compile(r'%s' % rule[1]['ignore'])
+        #running on an access point
+        from core.utils import iptables
+        if iptables().http is False and options.filter is None:
+            iptables().HTTP(options.listen_port)
 
-        except Exception, e:
-            sys.exit("[-] Error parsing airpwn config file: %s" % e)
+            #Setup iptables to block all not allowed traffic
+            self._block_traffic()
 
-        if False:
-            #TODO: if options.portaldns
-            t2 = threading.Thread(name='captive_dns_thread', target=self.captive_dns, args=(self.mon_interface,))
-            t2.setDaemon(True)
-            t2.start()
+    def parse_template(self, path):
+        if os.path.exists(path): # replace whole content
+            f = open(path, 'r')
+            data = f.read()
+            f.close()
+            return data
+        else:
+            sys.exit("[-] Error reading template file {}".format(path))
 
-    def captive_dns(self, iface):
-        sniff(filter="udp and port 53", prn=self.dns_callback, iface=iface)
 
-    def dns_callback(self, packet):
-
-        #pprint(packet, indent=2) #DEBUG
-        if packet.haslayer(UDP) and packet.haslayer(DNS):
-            if self._check_captured(packet[IP].src) \
-                    and packet[IP].src != self.ip:
-
-                req_domain = packet[DNS].qd.qname
-                #self.log.info("[*] DNSpwn {}".format(req_domain))
-
-                response = packet.copy()
-                response.FCfield = 2L
-                
-                response.src, response.dst = packet.dst, packet.src
-                response.sport, response.dport = packet.dport, packet.sport
-                # Set the DNS flags
-                response[DNS].qr = 1L
-                response[DNS].ra = 1L
-                response[DNS].ancount = 1
-                # Set hostname(CNAME) of captive portal
-                if self.portal_url:
-                    response[DNS].an = DNSRR(
-                        rrname = req_domain,
-                        type = 'CNAME',
-                        rclass = 'IN',
-                        ttl = 1,            #TODO: set TTL to minimal (0?)
-                        rdata = self.portal_url.hostname #set to Captive Portal Host or intercept with response
-                    )
-
-                # Set IP(A) for the response
-                else:
-                    return
-
-                    response[DNS].an = DNSRR(
-                        rrname = req_domain,
-                        type = 'A',
-                        rclass = 'IN',
-                        ttl = 1,            #TODO: set TTL to minimal (0?)
-                        rdata = self.dnspwn
-                    )
-
-                del response[IP].chksum
-                del response[UDP].chksum
-                del response[UDP].len
-                response = response.__class__(str(response))
-
-                #pprint(response, indent=2) #DEBUG
-
-                sendp(response, iface=self.mon_interface, verbose=False)
-                self.log.info("{} >> Spoofed DNS for {}".format(packet[IP].src, req_domain))
-
-    #def add_options(self, options):
     def options(self, options):
-        options.add_argument('--portalurl', type=str, dest='portalurl', help='URL of the captive Portal.')
-        #Define CP template
-
-    def responsestatus(self, request, version, code, message):
-        hn = request.headers['host']
-
-        if self.portal_url:
-            if not self.portal_url.hostname == hn:
-                self.log.info("{} setting 302 response header".format(hn))
-                #pprint(request, indent=2) #DEBUG
-
-                return {"request": request, "version": version, "code": 302, "message": "Found"}
-
-    def responseheaders(self, response, request):
-        ip = response.getClientIP()
-        hn = response.getRequestHostname()
-
-        if self.portal_url:
-            self.log.info("{} Vs {} ({})".format(self.portal_url.hostname, hn, request.uri))
-            if self.portal_url.hostname == hn:
-                #TODO: verify Ports
-                self.log.info("{} already browsing captive portal at {}".format(ip, response.uri))
-
-                return 
-            else:
-                #xx
-                self.log.info("{} setting location from {}".format(ip, response.uri))
-                #pprint(response, indent=2) #DEBUG
-
-                response.headers["Location"] = self.portal_url.geturl()
-                return {"response": response, "request": request}
-
-        return 
-    """
-    """
+        options.add_argument('--portalurl', type=str, dest='portalurl', help='URL of the captive-portal.')
+        options.add_argument('--portaldns', action='store_true', dest='portaldns', help='Hijack DNS records with captive-portal IP.')
+        options.add_argument('--portaltemplate', type=str, dest='portaltemplate', help='URL of the captive-portal.')
+        #TODO: Define captive-portal template
 
     def response(self, response, request, data):
-        encoding = None
         ip = response.getClientIP()
         hn = response.getRequestHostname()
-        self.log.info("{} requesting {}".format(ip, response.uri))
+        self.log.info("[response] {} requesting {}".format(ip, response.uri))
         
-        mime = ''
-        if 'Content-Type' in response.headers:
-            mime = response.headers['Content-Type']
-
-        # discover content encoding
-        if "charset" in mime:
-            match = re.search('charset=(.*)', mime)
-            if match:
-                encoding = match.group(1).strip().replace('"', "")
-            else:
-                try:
-                    encoding = chardet.detect(data)["encoding"]
-                except:
-                    pass
-        else:
-            try:
-                encoding = chardet.detect(data)["encoding"]
-            except:
-                pass
-
         # already requesting captive portal
         if self.portal_url:
             if self.portal_url.hostname == hn:
                 #FIXME: verify Ports
-                self.log.info("{} already browsing captive portal at {}".format(ip, response.uri))
-                self.log.info("{}".format(response))
+                self.log.info("[response] {} already browsing captive portal at {}".format(ip, response.uri))
 
                 return 
 
         #intercept 
-        #if self._check_captured(ip) \
-        #        and (hn not in self.ip) \
-        #        and ("text/html" in mime):
         if self._check_captured(ip) \
                 and (hn not in self.ip):
                     
             if self.portal_url:
-                self.log.info('{} redirecting from {} to captive portal at "{}"'.format(ip, hn, self.portal_url.geturl()))
+                self.log.info('[response] {} redirecting from {} to captive portal at "{}"'.format(ip, hn, self.portal_url.geturl()))
                 
-                if response.code != 302:
-                    self.log.info("Setting response code {}".format(response.code))
-                    response.code = 302
-
-                for x in response.headers:
-                    self.log.info("\t{}: {}".format(x,response.headers[x]))
-                    #Remove other headers?
-                    if x == 'expires' \
-                            or x == 'connection' \
-                            or x == 'content-type' \
-                            or x == 'content-length' \
-                            or x == 'date':
-                        del response.headers[x]
-                """
-                """
-
-                #response.headers = {}
+                # Set response code
+                response.code = 302
+                # Wipe headers
+                response.headers = {}
                 response.headers["Location"] = self.portal_url.geturl()
                 response.headers["Server"] = "Captive Portal"
-                response.headers["Pragma"] = "no-cache"
-                #response.headers["Content-Type"] = "text/html"
-                #response.headers["Content-Length"] = 0
 
-                for x in response.headers:
-                    self.log.info("{}: {}".format(x,response.headers[x]))
-                data = None
+                for x in response.headers: #DEBUG
+                    self.log.debug("[response] {}: {}".format(x,response.headers[x])) #DEBUG
+
+                data = ""
                 return {'response': response, 'request':request, 'data': data}
             else:
                 #TODO: paste content from CP template
                 html = self.portal_html
                 data = str(html)
 
-                #https://tools.ietf.org/html/rfc6585#section-6
+                # Set response code
+                #  see https://tools.ietf.org/html/rfc6585#section-6
                 response.code = 511
                 response.message = "Network Authentication Required"
+                # Wipe headers
+                response.headers = {}
+                response.headers["Location"] = self.portal_url.geturl()
+                response.headers["Server"] = "Captive Portal"
 
-
-                self.log.info("{} injecting custom html".format(ip, hn))
-                self.log.info("{}".format(response))
+                self.log.info("[response] {} injecting custom html".format(ip, hn))
 
                 return {'response': response, 'request':request, 'data': data}
-        #else:
-        #    self.log.info("%s >> not injecting HTTP request for %s" % (response.uri, hn))
         return
+
+    def modify(self, packet):
+        pkt = IP(packet.get_payload()) #converts the raw packet to a scapy compatible string
+
+        #TODO: Parse request and create response
+            # Send cooked response
+
+        packet.drop()
+
+
+    def captive_dns(self, iface):
+        #TODO: utilize nfqueue with reactor
+        #  see https://github.com/DanMcInerney/dnsspoof/blob/master/dnsspoof.py
+        #  see http://danmcinerney.org/reliable-dns-spoofing-with-python-scapy-nfqueue/
+        #  see http://danmcinerney.org/reliable-dns-spoofing-with-python-twisting-in-arp-poisoning-pt-2/
+        """
+        os.system('iptables -t nat -A PREROUTING -p udp --dport 53 -j NFQUEUE --queue-num 1')
+
+        nfqueue = NetfilterQueue()
+        nfqueue.bind(1, self.modify)
+        nfqueue.run()
+        """
+
+        #TODO: restrict filter
+        sniff(filter="udp and port 53", prn=self.dns_callback, iface=iface)
+
+    def dns_callback(self, packet):
+        # Is this a DNS Request
+        if packet.haslayer(UDP) \
+                and packet.haslayer(DNS) \
+                and packet.haslayer(DNSQR):
+            req_domain = packet[DNS].qd.qname
+
+            # Request for Address
+            if not packet.haslayer(DNSRR):
+                # A Record?
+                if packet[DNS].qd.qtype == 1:
+                    #TODO: Block legitimate DNS responses
+
+                    #pprint(packet, indent=2) #DEBUG
+
+                    # only spoof requests from target IPs
+                    if packet[IP].src == self.ip:
+                        pprint(packet, indent=2) #DEBUG
+                        self.log.info("[dns_callback] saw my own request {}".format(req_domain))
+
+                    elif packet[DNS].qd.qname != self.portal_url.hostname \
+                            and self._check_captured(packet[IP].src):
+
+                        query  = packet[DNS].qd
+                        answer = DNSRR(
+                                rrname = packet[DNS].qd.qname,
+                                type   = 'A',
+                                rclass = 'IN',
+                                ttl    = 1,
+                                rdata  = self.portal_ip
+                                )
+
+                        # setting hostname(CNAME) of captive portal
+                        if self.portal_url:
+                            answer = answer / DNSRR(
+                                    rrname = packet[DNS].qd.qname,
+                                    type   = 'CNAME',
+                                    rclass = 'IN',
+                                    ttl    = 1,
+                                    rdata  = self.portal_url.hostname
+                                    )
+
+                        # see https://github.com/gkarlos/vu-security/blob/a8b0a6e962f12b1d18ca6ee8bc11b042b7fcd3b4/DNSpoison.py
+                        response = Ether()
+                        response = response / IP(dst=packet[IP].src, src=packet[IP].dst)
+                        response = response / UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)
+                        response = response / DNS(
+                                id      = packet[DNS].id,
+                                qd      = query,
+                                qr      = 1L,
+                                ra      = 1L,
+                                an      = answer)
+                        
+                        try:
+                            #response.show2() #DEBUG
+                            #pprint(response, indent=2) #DEBUG
+                            sendp(response, iface=self.mon_interface, verbose=False)
+                        except Exception as e:
+                            #Issue: https://github.com/byt3bl33d3r/MITMf/issues/109
+                            self.log.error("[dns_callback] Error while sending spoofed DNS response >{}<".format(e))
+                        self.log.info("[dns_callback] {} >> Spoofed DNS for {}".format(packet[IP].src, req_domain))
+                    else:
+                        self.log.info("[dns_callback] {} Vs {}".format(packet[DNS].qd.qname, self.portal_url.hostname))
+                        self.log.info("[dns_callback] {} >> not a target DNS request for {}".format(packet[IP].src, req_domain))
+                else:
+
+                    #pprint(packet, indent=2) #DEBUG
+                    self.log.info("[dns_callback] {} >> Wrong DNS query type for {}({})".format(packet[IP].src, req_domain, packet[DNS].qd.qtype))
+            else:
+                #FIXME: Race condition, should drop original Response
+
+                #pprint(packet, indent=4) #DEBUG
+                self.log.info("[dns_callback] {} >> DNS response for {}".format(packet[IP].src, req_domain))
+        else:
+            #pprint(packet, indent=2) #DEBUG
+            self.log.info("[dns_callback] >> no DNS request")
 
     def on_shutdown(self):
         '''This will be called when shutting down'''
-        #os.system('iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j block')
-        os.system('iptables -F && iptables -X && iptables -t nat -F && iptables -t nat -X')
+        from core.utils import iptables
+        iptables().flush()
+
         pass
 
     def _check_captured(self, ip):
         #only inject once
-        self.log.info("{} registered? {}".format(ip, ip in self.dtable))
+        self.log.debug("[_check_captured] {} registered? {}".format(ip, ip in self.dtable))
         return not ip in self.dtable
 
     def _block_traffic(self):
-        self.log.info("Setting up iptables")
-        os.system('iptables -A INPUT -p tcp --destination-port 443 -j REJECT')
+        self.log.debug("Setting up iptables")
+        #TODO: limit to target IPs
+        os.system('iptables -t nat -A PREROUTING -p tcp --destination-port 443 -j REJECT')
         
 
     def _check_walledgarden(self, request):
